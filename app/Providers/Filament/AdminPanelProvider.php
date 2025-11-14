7b8892a0f40701c650ea035ab1af06b751e5f0ef
Johny <batenko4@gmail.com>
2025-11-14 10:59:22 +0200

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Models\User;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -17,6 +18,7 @@ use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AdminPanelProvider extends PanelProvider
@@ -28,6 +30,19 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->authenticateUsing(function ($request) {
+                $user = User::query()->where('email', $request->email)->first();
+
+                if (! $user || ! Hash::check($request->password, $user->password)) {
+                    return null;
+                }
+
+                if (! $user->role_id == 1) {
+                    return null;
+                }
+
+                return $user;
+            })
             ->colors([
                 'primary' => Color::Amber,
             ])
